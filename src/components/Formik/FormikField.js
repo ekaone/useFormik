b5ff1fbc430e7f8bcdd6a24ee616846ff2e7a47a
Eka <ekaone3033@gmail.com>
2019-11-08 16:07:54 +0700

    😀

--- src/components/Formik/FormikField.js
+++ src/components/Formik/FormikField.js
@@ -1,23 +1,52 @@
 import React from 'react';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, useField } from 'formik';
 import { 
   TextField,
   Button,
   Checkbox,
-  Radio 
+  Radio,
+  FormControlLabel  
 } from '@material-ui/core'
 import * as yup from "yup";
 
+
+const MyRadio = ({ label, ...props }) => {
+  const [field] = useField(props)
+  return <FormControlLabel {...field} control={<Radio />} />
+}
+
+const MyTextField = ({ placeholder, ...props }) => {
+  const [field, meta] = useField(props)
+  const errorText = meta.error && meta.touched ? meta.error : ""
+  return (
+    <TextField 
+      placeholder={placeholder}
+      {...field}
+      helperText={errorText}
+      error={!!errorText}
+    />
+  )
+}
 
 const Basic = () => (
   <>
     <Formik
       initialValues={{ 
+        middleName: '',
         firstName: '', 
         lastName: '',
         isTall: false,
         cookies: [],
         yogurt: ""  
+      }}
+      validate={values => {
+        const errors = {}
+        
+        if(values.middleName.includes("bob")) {
+          errors.middleName = "no bob"
+        }
+
+        return errors
       }}
       onSubmit={(data, { setSubmitting })=> {
         setSubmitting(true)
@@ -26,14 +55,17 @@ const Basic = () => (
         setSubmitting(false)
       }}
     >
-      {({ values, isSubmitting }) => (
+      {({ values, errors, isSubmitting }) => (
         <Form>
-          <Field 
+          <MyTextField placeholder="Middle name" name="middleName" />
+          <div>
+          <Field
             placeholder='First name'
             name='firstName'
             type='input'
             as={TextField}
           />
+          </div>
           <div>
           <Field 
             placeholder='Last name'
@@ -51,11 +83,13 @@ const Basic = () => (
             <Field name="yogurt" value="apple" type="radio" as={Radio} />
             <Field name="yogurt" value="pine apple" type="radio" as={Radio} />
             <Field name="yogurt" value="banana" type="radio" as={Radio} />
+            <MyRadio name="yogurt" type="radio" value="blueberry" label="Blueberry" />
           <div>
             <Button disabled={isSubmitting} type='submit'>Submit</Button>
           </div>
           <pre>{JSON.stringify({
             values: values,
+            errors: errors,
             cookiesLength: values.cookies.length
           }, null, 2)}</pre>
         </Form>
