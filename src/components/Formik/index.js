c5830fdf823cafa02f7cde79ad09e61feaf3227e
Eka Prasetia <ekaone3033@gmail.com>
2020-06-11 19:01:58 +0700

    small udpate

--- src/components/Formik/index.js
+++ src/components/Formik/index.js
@@ -1,6 +1,6 @@
-// export { default } from './FormikField'
-// export { default } from './FormikTextField'
+// export { default } from "./FormikField";
+// export { default } from "./FormikTextField";
 // export { default } from './Formik'
 // export { default } from './FormikAxios'
-// export { default } from './FormikMui'
-export { default } from './FormikProject'
+export { default } from "./FormikProject";
+// export { default } from "./FormikView";
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,1 @@
-export { default as Formik } from './Formik'
+export { default as Formik } from "./Formik";
